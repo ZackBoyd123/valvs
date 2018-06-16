d82390bd3d955ce99a580ade6455093830b67739
Richard Orton <richard.orton@glasgow.ac.uk>
2018-06-16 20:05:14 +0100

    Handle unpaired reads

--- valvs_ribopicker.sh
+++ valvs_ribopicker.sh
@@ -6,7 +6,7 @@ FLD=${PWD##*/}
 LOG="${FLD}_valvs_log.txt"
 touch $LOG
 
-while getopts ::1:2:t:k: TEST; do
+while getopts ::1:2:t:k:u: TEST; do
         case $TEST in
         
 	1) OPT_1=$OPTARG
@@ -16,6 +16,8 @@ while getopts ::1:2:t:k: TEST; do
         t) OPT_T=$OPTARG
         ;;
 	k) OPT_T=$OPTARG
+        ;;
+	u) OPT_T=$OPTARG
         ;;
         esac
 done
@@ -28,45 +30,54 @@ then
 fi
 
 . valvs_config.txt
+. valvs_check_reads.sh
 
-if [ -z $OPT_1 ]
-then
-        OPT_1=$FLD"_R1_valvs.fq"
-fi
-if [ -z $OPT_2 ]
-then
-        OPT_2=$FLD"_R2_valvs.fq"
-fi
 if [ -z $OPT_T ]
 then
         OPT_T=$config_threads
 fi
 
-echo "R1 = ${OPT_1} R2 = ${OPT_2} T = ${OPT_T}"
-echo "$(date) $config_number valvs_ribopicker.sh 1=$OPT_1 2=$OPT_2 t=$OPT_T" >> $LOG
+if [ -z $OPT_U ]
+then
+	echo "R1 = ${OPT_1} R2 = ${OPT_2} T = ${OPT_T}"
+	echo "$(date) $config_number valvs_ribopicker.sh 1=$OPT_1 2=$OPT_2 t=$OPT_T" >> $LOG
 
-#Interleave the two fastq files into 1 file - as ribo picker does not do paired end
-java -jar $config_ortools -i ${OPT_1} ${OPT_2} valvs_unmap
+	#Interleave the two fastq files into 1 file - as ribo picker does not do paired end
+	java -jar $config_ortools -i ${OPT_1} ${OPT_2} valvs_unmap
 
-ribopicker.pl -t ${OPT_T} -id valvs_ribo -f valvs_unmap.fastq -dbs ssr123,slr123
+	ribopicker.pl -t ${OPT_T} -id valvs_ribo -f valvs_unmap.fastq -dbs ssr123,slr123
 
-#De-interleave - removes unmatched pairs
-java -jar $config_ortools -d valvs_ribo_nonrrna.fq
+	#De-interleave - removes unmatched pairs
+	java -jar $config_ortools -d valvs_ribo_nonrrna.fq
 
-mv valvs_ribo_nonrrna_1.fastq $FLD"_R1_valvs.fq"
-mv valvs_ribo_nonrrna_2.fastq $FLD"_R2_valvs.fq"
+	mv valvs_ribo_nonrrna_1.fastq $FLD"_R1_valvs.fq"
+	mv valvs_ribo_nonrrna_2.fastq $FLD"_R2_valvs.fq"
+	
+	rm -f valvs_unmap.fastq
+	rm -f valvs_ribo_nonrrna.fq
+	rm -f valvs_ribo_rrna.fq
+else
+	echo "RU = ${OPT_U} T = ${OPT_T}"
+	echo "$(date) $config_number valvs_ribopicker.sh U=$OPT_U t=$OPT_T" >> $LOG
+
+	ribopicker.pl -t ${OPT_T} -id valvs_ribo -f $OPT_U -dbs ssr123,slr123
+
+	mv valvs_ribo_nonrrna.fastq $FLD"_valvs.fq"
+fi
 
 if [ -z $OPT_K ]
 then
 	:
 else
 	mkdir -p Reads
-	cp $FLD"_R1_valvs.fq" Reads/ribo_R1.fastq
-	cp $FLD"_R2_valvs.fq" Reads/ribo_R2.fastq
+	
+	if [ -z $OPT_U ]
+	then
+		cp $FLD"_R1_valvs.fq" Reads/ribo_R1.fastq
+		cp $FLD"_R2_valvs.fq" Reads/ribo_R2.fastq
+	else
+		cp $FLD"_valvs.fq" Reads/ribo.fastq
+	fi
 fi
 
 valvs_readstats.sh
-
-rm -f valvs_unmap.fastq
-rm -f valvs_ribo_nonrrna.fq
-rm -f valvs_ribo_rrna.fq
