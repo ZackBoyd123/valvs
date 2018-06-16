e46adfcea5086c36c09e811579d3e529cf3053f6
Richard Orton <richard.orton@glasgow.ac.uk>
2018-06-16 21:05:47 +0100

    Update valvs.sh

--- valvs.sh
+++ valvs.sh
@@ -6,10 +6,10 @@ touch $LOG
 
 . valvs_config.txt
 
+echo "$(date) $config_version valvs.sh" >> $LOG
+
 valvs_setup_reads.sh
 valvs_setup_ref.sh
 valvs_trim_galore.sh
 valvs_tanoti.sh
 valvs_vphaser.sh
-
-echo "$(date) $config_version valvs.sh" >> $LOG
