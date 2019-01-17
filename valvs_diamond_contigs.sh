dec4ee5eeb00285e6102ee803417de85309f1405
Richard Orton <richard.orton@glasgow.ac.uk>
2019-01-17 17:02:53 +0000

    Option to run on any contigs
    override default Spades/contigs.fasta, added threads

--- valvs_diamond_contigs.sh
+++ valvs_diamond_contigs.sh
@@ -6,17 +6,25 @@ FLD=${PWD##*/}
 LOG="${FLD}_valvs_log.txt"
 touch $LOG
 
-while getopts :o:d: TEST; do
+while getopts :o:d:i:t: TEST; do
 	case $TEST in
 	o) OPT_O=$OPTARG
 	;;
 	d) OPT_D=$OPTARG
 	;;
+	i) OPT_I=$OPTARG
+	;;
+	t) OPT_T=$OPTARG
+	;;	
 	esac
 done
 
 . valvs_config.txt
 
+if [ -z $OPT_T ] 
+then
+	OPT_T=$config_threads
+fi
 if [ -z $OPT_O ] 
 then
 	OPT_O=${FLD}
@@ -25,11 +33,14 @@ if [ -z $OPT_D ]
 then
 	OPT_D=$config_diamond
 fi
+if [ -z $OPT_I ] 
+then
+	OPT_I=Spades/contigs.fasta
+fi
 
 echo "o = ${OPT_O} d=${OPT_D}"
 echo "$(date) $config_version valvs_diamond_contigs.sh o=$OPT_O d=$OPT_D" >> $LOG
 
-mkdir -p Spades/diam_temp
 mkdir -p diam_temp
-diamond blastx -d $OPT_D -f 6 -o Spades/${OPT_O}_diam_nr.txt -p 10 -q Spades/contigs.fasta -t diam_temp
-ktImportBLAST Spades/${OPT_O}_diam_nr.txt -o Spades/${OPT_O}_diam_nr.html > Spades/${OPT_O}_kt.txt
+diamond blastx -d $OPT_D -f 6 -o Spades/${OPT_O}_diam.txt -p ${OPT_T} -q $OPT_I} -t diam_temp
+ktImportBLAST Spades/${OPT_O}_diam.txt -o Spades/${OPT_O}_diam.html > Spades/${OPT_O}_kt.txt
