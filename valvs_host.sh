ee0535f635944b6f9ba7d9b8f18ad38abc2c53b9
Richard Orton <richard.orton@glasgow.ac.uk>
2019-01-17 16:02:54 +0000

    Added threads and alignment mode

--- valvs_host.sh
+++ valvs_host.sh
@@ -6,7 +6,7 @@ FLD=${PWD##*/}
 LOG="${FLD}_valvs_log.txt"
 touch $LOG
 
-while getopts :1:2:r:k:o TEST; do
+while getopts :1:2:r:k:o:t:m: TEST; do
 	case $TEST in
 
 	1) OPT_1=$OPTARG
@@ -19,6 +19,10 @@ while getopts :1:2:r:k:o TEST; do
 	;;
 	o) OPT_O=$OPTARG
 	;;
+	t) OPT_T=$OPTARG
+	;;	
+	m) OPT_M=$OPTARG
+	;;
 	esac
 done
 
@@ -26,9 +30,7 @@ if [[ $1 = "-h" ]]
 then
         printf "\t----${0##*/}----\n\t[-1]\tName Of First Fastq File\n\t[-2]\tName Of Second Fastq File\n\t[-r]\tReference File\n\t[-o]\tOutput File Name\n\t[-k]\t Keep Old Files? y\n"
 	printf "\n----------------------------------------\n"
-        printf "Maps reads to a host DB  using bowtie2 both
-paired and unpaired reads are supported aswell
-as different bowtie alignment modes."
+        printf "Maps reads to a host index using bowtie2 both paired and unpaired reads are supported aswell as different bowtie alignment modes."
         printf "\n----------------------------------------\n"
         exit 1
 
@@ -76,11 +78,21 @@ else
         R1=${OPT_O}_R1_valvs.fq
         R2=${OPT_O}_R2_valvs.fq
 fi
+if [ -z $OPT_T ] 
+then
+	OPT_T=$config_threads
+fi
+if [ -z $OPT_M ] 
+then
+	OPT_M="local"
+else
+	OPT_M="end-to-end"
+fi
 
-echo "Ref = $OPT_R R1 = $OPT_1 R2 = $OPT_2 OutputStub = $OPT_O"
-echo "$(date) $config_version valvs_host.sh r=$OPT_R o=$OPT_O 1=$OPT_1 2=$OPT_2" >> $LOG
+echo "Ref = $OPT_R R1 = $OPT_1 R2 = $OPT_2 OutputStub = $OPT_O Threads = $OPT_T Alignment Mode = $OPT_M"
+echo "$(date) $config_version valvs_host.sh r=$OPT_R o=$OPT_O 1=$OPT_1 2=$OPT_2 t=$OPT_T m=$OPT_M" >> $LOG
 
-valvs_bowtie2.sh -r $OPT_R -o ${OPT_O} -1 $OPT_1 -2 $OPT_2
+valvs_bowtie2.sh -r $OPT_R -o ${OPT_O} -1 $OPT_1 -2 $OPT_2 -t $OPT_T -m $OPT_M
 valvs_extract_unmapped.sh -b ${OPT_O}.bam -1 $R1 -2 $R2
 
 if [ -z $OPT_K ]
