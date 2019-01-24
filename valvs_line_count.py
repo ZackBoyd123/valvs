f5bee7ef6c89de5068e35f9413f6577be8178a32
ZackBoyd123 <zacharyboyd123@gmail.com>
2019-01-24 23:07:00 +0000

    Update valvs_line_count.py

--- valvs_line_count.py
+++ valvs_line_count.py
@@ -1,7 +1,7 @@
 import sys
 with open(sys.argv[1], "r") as f:
-    count = 1
-    for en, i in enumerate(f):
-        count += en
+    count = 0
+    for i in f:
+        count += 1
     f.close()
 print(count / 4)
